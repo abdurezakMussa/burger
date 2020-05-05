8c3eb3f3a56672b64fc79d9ffbea3d102c812fd2
AbduEth <abdurekmjob@gmail.com>
2020-05-05 10:53:20 -0400

    changes

--- controller/burger_controller.js
+++ controller/burger_controller.js
@@ -4,43 +4,40 @@ var router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
 
-var burgers=require("../models/burger.js");
+var burger=require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 
 router.get("/",function(req,res){
-    burgers.selectAll(function(data){
+    burger.selectAll(function(data){
         let hbsobj={
-            burger:data
+            burgers:data
         };
-        console.log("yes"+hbsobj)
+       // console.log("yes"+hbsobj)
         res.render("index",hbsobj);
     });
 });
 
-router.post("/api/burger",function(req,res){
-    burgers.insertOne(["burger_name","devoured"],[req.body.burger_name,req.body.devoured],function(result){
+router.post("/",function(req,res){
+    burger.insertOne(["burger_name","devoured"],[req.body.burger_name,req.body.devoured],function(){
         //send back the Id of the new burger 
-        res.json({id:result.insertId});
+        res.redirect("/");
     });
 });
 
-router.put("/api/burger/:id", function(req,res){
+router.put("/:id", function(req,res){
     let condition="id = "+ req.params.id;
     console.log("condition",condition);
-    burgers.updateOne(
+    burger.updateOne(
         {
             devoured: req.body.devoured
-        },
-        condition,
-        function(result){
-            if(result.changedRows===0){
-                 // If no rows were changed, then the ID must not exist, so 404
-                 return res.status(404).end();
-                }
-                res.status(200).end();
+        }, 
+        condition, function(){
+            res.redirect("/")
          }
     );
 });
 //export routes for server.js to use it.
 module.exports=router;
+
+
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var express = require("express");
-var PORT = 8080;
+var bodyParser=require("body-parser");
+var methodOverride=require("method-override")
 var app = express();
 
-
+var PORT = 8080;
 //// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
-
+app.use(methodOverride("_method"));
 // Parse request body as JSON
-app.use(express.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.json());
 
 // Set Handlebars.
@@ -17,7 +18,7 @@ app.set("view engine","handlebars");
 
 // Import routes and give the server access to them.
 var routes=require("./controller/burger_controller.js");
-app.use(routes);
+app.use("/",routes);
 
 ///Port listener 
 app.listen(PORT,function(){
