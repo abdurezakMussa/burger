cfdfd156f4a44c5d410ff14b9ea87888182d045b
AbduEth <abdurekmjob@gmail.com>
2020-05-07 18:17:08 -0400

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,21 @@
 // set up Mysql conncetion.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burger_db"
-  });
+var connection;
 
+if (process.env.JAWSDB_URL) {
+    // DB is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    // DB is local on localhost
+    connection = mysql.createConnection({
+      port: 3306,
+      host: 'localhost',
+      user: 'root',
+      password: "",
+      database: 'burgers_db'
+    })
+  };
 connection.connect(function(err){
     if(err){
         console.log("Error connecting: "+err.stack);
