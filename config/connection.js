0049c75133835bf6df91ce1902921f56a9f0aa48
AbduEth <abdurekmjob@gmail.com>
2020-05-09 21:45:12 -0400

    clean up

--- config/connection.js
+++ config/connection.js
@@ -13,7 +13,7 @@ if (process.env.JAWSDB_URL) {
       host: 'localhost',
       user: 'root',
       password: "",
-      database: 'burgers_db'
+      database: 'burger_db'
     });
  };
 connection.connect(function(err){
